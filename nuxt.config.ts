f3eefc6d143372341d40925a0050b526ebad0457
Daniele Tortora <daniele.tortora@snyk.io>
2024-11-10 16:19:53 +0100

    chore: adds assets and modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  pages: true,
+  modules: [
+    "nuxt-icon",
+    // "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
+    "@pinia/nuxt",
+    "@vite-pwa/nuxt",
+  ],
+  runtimeConfig: {
+    public: {
+      bucketUrl: import.meta.env.BUCKET_URL,
+    },
+  },
+  compatibilityDate: "2024-04-03",
+  devtools: { enabled: true },
+});
